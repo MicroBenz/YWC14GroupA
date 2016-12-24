3f5557b336f8fbe457b94a3624fc064023e053f9
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-12-24 10:36:50 +0700

    Add libs

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+    <link href="{{ asset('/libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1>AAAAAA</h1>
+        <button class="button">AAA</button>
+    </div>
+
+    <script src="{{ asset('/libs/jquery/jquery-2.2.4.min.js') }}">
+    <script src="{{ asset('/libs/bootstrap/js/bootstrap.min.js') }}">
+    
+</body>
+</html>
