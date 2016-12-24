68a7689e6793b5aee36698e95a4a98c32399cf60
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2016-12-24 13:56:55 +0700

    NOOO

--- resources/views/search-result.blade.php
+++ resources/views/search-result.blade.php
@@ -8,6 +8,24 @@
                 <img src="{{ asset('/assets/img/people1.jpg') }}">
             </div>
             <div class="result-content">
+                <h1 class="result-title">ประสบการณ์การเรียนวิศวฯ จุฬาฯ</h1>
+                <p class="result-description">"Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem"</p>
+                <div class="bottom-group">
+                    <div class="tags-group">
+                        <a class="pill-button tag">architech</a>
+                        <a class="pill-button tag">Cyber</a>
+                        <a class="pill-button tag">ICT</a>
+                        <a class="pill-button tag">engineer</a>
+                    </div>
+                    <a class="pill-button read-more">See more</a>
+                </div>
+            </div>
+        </div>
+        <div class="result-card">
+            <div class="result-image">
+                <img src="{{ asset('/assets/img/people1.jpg') }}">
+            </div>
+            <div class="result-content">
                 <h1 class="result-title">Lorem Ipsum</h1>
                 <p class="result-description">"Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem"</p>
                 <div class="bottom-group">
