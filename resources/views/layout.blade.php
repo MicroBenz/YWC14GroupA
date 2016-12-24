50251ff54ac1beb9199ec2f9b0243cd59b41c89f
Thanyakorn Daengkasem <kornny24159@gmail.com>
2016-12-24 10:40:37 +0700

    set default bootstrap template

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>Laravel</title>
-    <link href="{{ asset('/libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/libs/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 </head>
 <body>
     <div class="container">
@@ -10,8 +10,8 @@
         <button class="button">AAA</button>
     </div>
 
-    <script src="{{ asset('/libs/jquery/jquery-2.2.4.min.js') }}">
-    <script src="{{ asset('/libs/bootstrap/js/bootstrap.min.js') }}">
+    <script src="{{ asset('assets/libs/jquery/jquery-2.2.4.min.js') }}">
+    <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.min.js') }}">
     
 </body>
 </html>
